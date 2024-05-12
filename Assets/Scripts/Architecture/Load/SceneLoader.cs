82b3ef87b07c0f1c652feb1890d6c307ba409d62
Данила Косынкин <danilakosynkin@mail.ru>
2024-05-12 23:49:34 +0300

    Update yandex plugin

--- Assets/Scripts/Architecture/Load/SceneLoader.cs
+++ Assets/Scripts/Architecture/Load/SceneLoader.cs
@@ -5,11 +5,6 @@ namespace Code
 {
     public class SceneLoader
     {
-        public void LoadHome()
-        {
-            SceneManager.LoadScene(0);
-        }
-        
         public void LoadGame(int indexLoad)
         {
             if(SceneManager.sceneCountInBuildSettings <= indexLoad || YandexGame.savesData._maxLevel < indexLoad)
--- Assets/Scripts/UI/Buttons/TapStartGameButton.cs
+++ Assets/Scripts/UI/Buttons/TapStartGameButton.cs
@@ -10,18 +10,26 @@ namespace Code
 
         private LevelTimer _levelTimer;
         private PagesController _pagesController;
+        private GameManager _gameManager;
 
         [Inject]
-        private void Construct(LevelTimer levelTimer, PagesController pagesController)
+        private void Construct(LevelTimer levelTimer, PagesController pagesController, GameManager gameManager)
         {
+            _gameManager = gameManager;
             _levelTimer = levelTimer;
             _pagesController = pagesController;
+        }
+
+        private void OnEnable()
+        {
+            _gameManager.PauseGame();
         }
 
         protected override void OnClick()
         {
             base.OnClick();
 
+            _gameManager.PlayGame();
             _levelTimer.StartTimer();
             _pagesController.ShowPage(_pageId);
         }
